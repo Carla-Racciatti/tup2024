a775edd81abd8f9c7adec0043ccd2164024501bd
Carla-Racciatti <racciatti.carla@gmail.com>
2024-05-13 10:49:51 -0300

    Cambios en archivo pom y Agrego nuevas clases

--- Tp2Carla/src/ar/edu/utn/frbb/tup/main/java/InputProcessors/BaseInputProcessor.java
+++ Tp2Carla/src/ar/edu/utn/frbb/tup/main/java/InputProcessors/BaseInputProcessor.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 
 public class BaseInputProcessor {
     protected Scanner scanner = new Scanner(System.in);
-
-
     protected static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
